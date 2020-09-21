fd0bfa7f51580beac1a802ed1662de4e2cc77001
rosig <rglj2@cin.ufpe.br>
2020-09-20 21:44:30 -0300

    Alterando router basename

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
@@ -14,17 +14,15 @@ function App() {
   return (
     <div className="App">
       <Header setShowSidebar={setShowSidebar} />
-      <Sidebar
-        showSidebar={showSidebar}
-        setShowSidebar={setShowSidebar} />
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <Content />
     </div>
   );
 }
 
 ReactDOM.render(
-  <BrowserRouter basename="/rosig-apps/">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
