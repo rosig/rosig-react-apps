f2c96251c2506257273eaaf2ebcdcf88c8dec6c0
Rosinaldo Guedes <rglj2@cin.ufpe.br>
2022-09-24 16:50:40 -0300

    Atualiza package.json, ajusta lerp-rings e adiciona favicon e meta image

--- src/Pages/LerpRings/Line.js
+++ src/Pages/LerpRings/Line.js
@@ -1,39 +1,47 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef } from "react";
 
 import * as THREE from "three";
 
 import { useFrame, useThree } from "react-three-fiber";
 
 const Line = ({ mouse }) => {
-  const { viewport } = useThree();
+	const { viewport } = useThree();
+	const lineRef = useRef();
 
-  const startP = useMemo(() => new THREE.Vector3(0, 0, 0), []);
-  const target = useMemo(() => new THREE.Vector3(0, 0, 0), []);
+	const startP = useMemo(() => new THREE.Vector3(0, 0, 0), []);
+	const target = useMemo(() => new THREE.Vector3(0, 0, 0), []);
 
-  const next_x = useMemo(() => mouse.x * (viewport.width / 2), [
-    mouse.x,
-    viewport.width,
-  ]);
+	const next_x = useMemo(
+		() => mouse.x * (viewport.width / 2),
+		[mouse.x, viewport.width]
+	);
 
-  const next_y = useMemo(() => mouse.y * (viewport.height / 2), [
-    mouse.y,
-    viewport.height,
-  ]);
+	const next_y = useMemo(
+		() => mouse.y * (viewport.height / 2),
+		[mouse.y, viewport.height]
+	);
 
-  useFrame(() => {
-    if (next_x !== target.x) {
-      target.x = next_x;
-      target.y = next_y;
-    }
-    // console.log(target);
-  });
+	const lineGeometry = new THREE.BufferGeometry().setFromPoints([
+		startP,
+		target,
+	]);
 
-  return (
-    <line>
-      <geometry attach="geometry" vertices={[startP, target]} onUpdate={self => (self.verticesNeedUpdate = true)} />
-      <lineBasicMaterial attach="material" color="#F20574" />
-    </line>
-  );
+	useFrame(() => {
+		if (next_x !== target.x) {
+			target.x = next_x;
+			target.y = next_y;
+		}
+
+		lineGeometry.setFromPoints([startP, target]);
+	});
+
+	return (
+		<line>
+			<line geometry={lineGeometry} ref={lineRef}>
+				<lineBasicMaterial attach="material" color="#F20574" />
+			</line>
+		</line>
+	);
 };
 
 export default Line;
